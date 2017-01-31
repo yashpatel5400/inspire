d33bbd60ab7e0ef9a4a5ead3c474b7c183ee3145
Yash Patel <ypatel@princeton.edu>
2017-01-30 21:09:40 -0500

    Added basic outline of code base

--- inspire.go
+++ inspire.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	QUICK     := "1"
+	SCHEDULE  := "2"
+	CUSTOMIZE := "3"
+	FAVORITES := "4"
+	ABOUT     := "5"
+
+	fmt.Println(`Please select from the following:
+		(1) Quick
+		(2) Schedule
+		(3) Customize
+		(4) Favorites
+		(5) About`)
+
+	reader  := bufio.NewReader(os.Stdin)
+	mode, _ := reader.ReadString('\n')
+	
+	switch mode {
+    	case QUICK:
+    		quick()
+
+    	case SCHEDULE:
+    		quick()
+
+		case CUSTOMIZE:
+    		quick()
+
+		case FAVORITES:
+    		quick()
+
+		case ABOUT:
+    		quick()
+    }
 }
---
+++ quick.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func quick() {
+	fmt.Println("Poop")
+}
